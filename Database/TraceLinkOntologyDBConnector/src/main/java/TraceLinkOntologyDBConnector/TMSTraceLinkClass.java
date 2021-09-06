004879bcf5f37f577985512f2c2d0be912361496
xht308 <xht308@outlook.com>
2021-09-06 19:44:42 +0800

    Formating the code

--- Database/TraceLinkOntologyDBConnector/src/main/java/TraceLinkOntologyDBConnector/TMSTraceLinkClass.java
+++ Database/TraceLinkOntologyDBConnector/src/main/java/TraceLinkOntologyDBConnector/TMSTraceLinkClass.java
@@ -2,12 +2,12 @@ package TraceLinkOntologyDBConnector;
 
 //Purpose: This class is designed to represent a trace link type in the program
 //The instances of this class are typically created by RDFOntology or TMSDBManager (included in DBOntology objects)
-public class TMSTraceLinkClass extends TMSTraceLinkObject{
-	
+public class TMSTraceLinkClass extends TMSTraceLinkObject {
+
 	public TMSTraceLinkClass(String iri, String name) {
 		super(iri, name);
 	}
-	
+
 	public TMSTraceLinkClass(String iri) {
 		super(iri);
 	}
--- Database/TraceLinkOntologyDBConnector/src/main/java/TraceLinkOntologyDBConnector/TMSTraceLinkType.java
+++ Database/TraceLinkOntologyDBConnector/src/main/java/TraceLinkOntologyDBConnector/TMSTraceLinkType.java
@@ -2,12 +2,12 @@ package TraceLinkOntologyDBConnector;
 
 //Purpose: This class is designed to represent a trace link type in the program
 //		The instances of this class are typically created by RDFOntology or TMSDBManager (included in DBOntology objects)
-public class TMSTraceLinkType extends TMSTraceLinkObject{
-	
+public class TMSTraceLinkType extends TMSTraceLinkObject {
+
 	public TMSTraceLinkType(String iri, String name) {
 		super(iri, name);
 	}
-	
+
 	public TMSTraceLinkType(String iri) {
 		super(iri);
 	}
